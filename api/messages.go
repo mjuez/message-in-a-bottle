dce3e07d0d7c95df7ec6cbad8013bfdb2fe5e6b8
Mario <mjuezath@gmail.com>
2023-09-24 10:30:49 +0000

    renamed things and improved regex

--- api/messages.go
+++ api/messages.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"path/filepath"
 	"regexp"
 	"time"
 )
 
-type Message struct {
-	Date     string `json:"date"`
-	ImageUrl string `json:"image_url"`
-	TextUrl  string `json:"text_url"`
+type Bottle struct {
+	Date       string `json:"date"`
+	ImageUrl   string `json:"image_url"`
+	MessageUrl string `json:"message_url"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -38,39 +37,39 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	bodyText := string(bodyBytes)
-	regex := `"name": "([^"]+\.(jpg|jpeg|png|gif))"`
+	regex := `"name": "((([^"\_]+)(\_\d+)?)\.(jpg|jpeg|png|gif))"`
 	compiledRegex := regexp.MustCompile(regex)
 	matches := compiledRegex.FindAllStringSubmatch(bodyText, -1)
-	messages := []Message{}
+	bottles := []Bottle{}
 	for _, match := range matches {
-		if len(match) >= 2 {
-			message := match[1]
-			format := filepath.Ext(message)
-			messageName := message[:len(message)-len(format)]
+		if len(match) >= 4 {
+			fmt.Println(match)
+			bottle := match[1]
+			bottleNoExt := match[2]
+			date := match[3]
 
-			date, err := time.Parse("060102", messageName)
+			parsedDate, err := time.Parse("060102", date)
 			if err != nil {
 				fmt.Println("Cannot analyse date:", err)
-				return
+			} else {
+				jsonBottle := Bottle{
+					Date:       parsedDate.Format("02/01/06"),
+					ImageUrl:   url + bottle,
+					MessageUrl: url + bottleNoExt + ".txt",
+				}
+				bottles = append(bottles, jsonBottle)
 			}
-
-			jsonMessage := Message{
-				Date:     date.Format("02/01/06"),
-				ImageUrl: url + message,
-				TextUrl:  url + messageName + ".txt",
-			}
-			messages = append(messages, jsonMessage)
 		}
 	}
 
-	numMessages := len(messages)
-	invertedMessages := make([]Message, numMessages)
-	for i, message := range messages {
-		invertedMessages[numMessages-1-i] = message
+	numBottles := len(bottles)
+	invertedBottles := make([]Bottle, numBottles)
+	for i, bottle := range bottles {
+		invertedBottles[numBottles-1-i] = bottle
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	encodingError := json.NewEncoder(w).Encode(invertedMessages)
+	encodingError := json.NewEncoder(w).Encode(invertedBottles)
 	if encodingError != nil {
 		http.Error(w, encodingError.Error(), http.StatusInternalServerError)
 		return
